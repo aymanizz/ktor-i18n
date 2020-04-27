7578eae97ba2fb4c995932a68afa917548bc3ce5
Ayman Izzeldin <ayman.izzeldin@gmail.com>
2020-04-27 20:51:05 +0200

    add dokka task for generating documentations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.dokka.gradle.DokkaTask
 
 plugins {
     kotlin("jvm") version "1.3.70"
+    maven
+    id("org.jetbrains.dokka") version "0.10.1"
 }
 
 group = "com.github.aymanizz"
@@ -10,6 +13,7 @@ version = "0.1.0"
 repositories {
     mavenCentral()
     maven("https://dl.bintray.com/kotlin/ktor")
+    maven("https://dl.bintray.com/kotlin/dokka")
 }
 
 dependencies {
@@ -19,10 +23,17 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.6.2")
 }
 
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
 tasks.test {
     useJUnitPlatform()
 }
 
-tasks.withType<KotlinCompile>().configureEach {
-    kotlinOptions.jvmTarget = "1.8"
+tasks {
+    getting(DokkaTask::class) {
+        outputFormat = "html"
+        outputDirectory = "$buildDir/docs"
+    }
 }

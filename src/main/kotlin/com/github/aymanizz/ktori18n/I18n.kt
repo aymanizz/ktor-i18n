412be852221009748e799bd1586aba466119b5e3
atistrcsn <atistrcsn@users.noreply.github.com>
2020-06-25 10:36:32 +0200

    first attempt tor redirect by language pathPrefix -> working for us

--- src/main/kotlin/com/github/aymanizz/ktori18n/I18n.kt
+++ src/main/kotlin/com/github/aymanizz/ktori18n/I18n.kt
@@ -1,8 +1,18 @@
 package com.github.aymanizz.ktori18n
 
-import io.ktor.application.*
+import com.github.aymanizz.ktori18n.I18n.Configuration
+import io.ktor.application.Application
+import io.ktor.application.ApplicationCall
+import io.ktor.application.ApplicationCallPipeline
+import io.ktor.application.ApplicationFeature
+import io.ktor.application.feature
+import io.ktor.features.origin
+import io.ktor.http.Cookie
 import io.ktor.request.acceptLanguage
+import io.ktor.response.respondRedirect
 import io.ktor.util.AttributeKey
+import io.ktor.util.KtorExperimentalAPI
+import io.ktor.util.pipeline.PipelineContext
 import java.util.*
 
 /**
@@ -10,11 +20,23 @@ import java.util.*
  *
  * @see Configuration for how to configure this feature
  */
-class I18n private constructor(configuration: Configuration): MessageResolver by configuration.messageResolver {
+class I18n private constructor(configuration: Configuration) : MessageResolver by configuration.messageResolver {
+
     val supportedLocales = configuration.supportedLocales
     val defaultLocale = configuration.defaultLocale ?: supportedLocales.first()
 
-    init { Locale.setDefault(defaultLocale) }
+    val useOfCookie = configuration.useOfCookie
+    val localeCookieName = configuration.cookieName
+
+    val useOfRedirection = configuration.useOfRedirection
+    val supportedPathPrefixes = "(${supportedLocales.joinToString("|", transform = { it.language })})".toRegex()
+
+    @KtorExperimentalAPI
+    val excludePredicates: List<(ApplicationCall) -> Boolean> = configuration.excludePredicates.toList()
+
+    init {
+        Locale.setDefault(defaultLocale)
+    }
 
     /**
      * The configuration for the internationalization feature.
@@ -27,16 +49,71 @@ class I18n private constructor(configuration: Configuration): MessageResolver by
      *
      * [messageResolver] instance of MessageResolver used in message resolution. Defaults to
      * [ResourceBundleMessageResolver].
-     *
      * @see MessageResolver
+     *
+     * [useOfCookie] Whether to use cookie or not to resolve [Locale]
+     * [cookieName] A sensible naming of the cookie
+     *
+     * [useOfRedirection] Whether to use redirection based on path language prefix
+     *
+     * [excludePredicates] The list of call predicates for redirect exclusion.
+     * Any call matching any of the predicates will not be redirected by this feature.
+     *
      */
     class Configuration {
+
         lateinit var supportedLocales: List<Locale>
         var defaultLocale: Locale? = null
         var messageResolver: MessageResolver = ResourceBundleMessageResolver()
+
+        var useOfCookie: Boolean = false
+        var cookieName: String = "locale"
+
+        var useOfRedirection: Boolean = false
+
+        @KtorExperimentalAPI
+        val excludePredicates: MutableList<(ApplicationCall) -> Boolean> = ArrayList()
+
+        /**
+         * Exclude calls with paths matching the [pathPrefixes] from being redirected with language prefix by this feature.
+         */
+        @KtorExperimentalAPI
+        fun excludePrefixes(vararg pathPrefixes: String) {
+            pathPrefixes.forEach { prefix ->
+                exclude { call -> call.request.origin.uri.startsWith(prefix) }
+            }
+        }
+
+        /**
+         * Exclude calls matching the [predicate] from being redirected with language prefix by this feature.
+         * @see io.ktor.features.HttpsRedirect for example of exclusions
+         */
+        @KtorExperimentalAPI
+        fun exclude(predicate: (call: ApplicationCall) -> Boolean) {
+            excludePredicates.add(predicate)
+        }
     }
 
+    @KtorExperimentalAPI
+    private suspend fun intercept(ctx: PipelineContext<Unit, ApplicationCall>) {
+        val call = ctx.context
+        val language = call.locale.language
+
+        // todo: maybe using regex would be faster and simpler than working w/ lists
+        val uri = call.request.origin.uri.trimStart('/').trimEnd('/').split('/')
+        if (!uri.first().matches(supportedPathPrefixes)
+            && excludePredicates.none { predicate -> predicate(call) }
+        ) {
+            val toRedirect =
+                mutableListOf<String>(language, *uri.toTypedArray()).joinToString("/", prefix = "/").trimEnd('/')
+            call.respondRedirect(toRedirect)
+            ctx.finish()
+        }
+    }
+
+    @KtorExperimentalAPI
     companion object Feature : ApplicationFeature<ApplicationCallPipeline, Configuration, I18n> {
+
         override val key = AttributeKey<I18n>("I18n")
 
         override fun install(pipeline: ApplicationCallPipeline, configure: Configuration.() -> Unit): I18n {
@@ -46,32 +123,72 @@ class I18n private constructor(configuration: Configuration): MessageResolver by
                     "default locale must be one of the supported locales"
                 }
             }
-            return I18n(configuration)
+
+            val feature = I18n(configuration)
+
+            if (configuration.useOfRedirection) {
+                pipeline.intercept(ApplicationCallPipeline.Features) { feature.intercept(this) }
+            }
+
+            return feature
         }
     }
 }
 
-
-val Application.i18n get() = feature(I18n)
+@KtorExperimentalAPI
+val Application.i18n
+    get() = feature(I18n)
 
 private val CallLocaleKey = AttributeKey<Locale>("CallLocale")
 
 /**
- * The locale for the current call, from the accept language header.
+ * The locale for the current call, based on accept language header, then from the optional language cookie and finally from
+ * the language prefix of the request path.
  *
  * If there is no supported locale that matches the request accept language locales, the the default locale is returned.
  */
-val ApplicationCall.locale get() = attributes.computeIfAbsent(CallLocaleKey) {
-    val i18n = application.i18n
-    val acceptLocales = request.acceptLanguage()
-    val ranges = if (acceptLocales != null) Locale.LanguageRange.parse(acceptLocales) else listOf()
-    Locale.filter(ranges, i18n.supportedLocales).firstOrNull()
+@KtorExperimentalAPI
+val ApplicationCall.locale
+    get() = attributes.computeIfAbsent(CallLocaleKey) {
+        val i18n = application.i18n
+
+        fun createCooke(locale: Locale) {
+            response.cookies.append(Cookie(i18n.localeCookieName, locale.language, maxAge = 60 * 60))
+        }
+
+        // check header first
+        val acceptLocales = request.acceptLanguage()
+        val ranges = if (acceptLocales != null) Locale.LanguageRange.parse(acceptLocales) else listOf()
+        var locale = Locale.filter(ranges, i18n.supportedLocales).firstOrNull()
             ?: Locale.lookup(ranges, i18n.supportedLocales)
             ?: i18n.defaultLocale
-}
+
+        var localeCookie: String? = null
+        if (i18n.useOfCookie) {
+            // cookie always overrides the accept language header
+            localeCookie = request.cookies[i18n.localeCookieName]
+            localeCookie?.let { locale = Locale.forLanguageTag(it) }
+        }
+
+        if (i18n.useOfRedirection) {
+            // URI language prefix always overrides cookie resolving
+            // todo: maybe using regex would be faster and simpler than working w/ lists
+            val uri = request.origin.uri.trimStart('/').trimEnd('/').split('/')
+            val languagePrefix = uri.first()
+            if (languagePrefix.matches(i18n.supportedPathPrefixes)) {
+                locale = Locale.forLanguageTag(languagePrefix)
+                if (i18n.useOfCookie && languagePrefix != localeCookie) createCooke(locale)
+            }
+        }
+
+        if (i18n.useOfCookie && localeCookie == null) createCooke(locale)
+
+        locale
+    }
 
 /**
  * Helper function for resolving a message using the call locale as returned from [ApplicationCall.locale].
  */
+@KtorExperimentalAPI
 fun ApplicationCall.t(keys: Iterable<String>, vararg args: Any = arrayOf()) =
-        application.i18n.t(locale, keys, *args)
+    application.i18n.t(locale, keys, *args)
